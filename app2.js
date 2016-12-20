f19ee6beed6b62050a933de46e35e43336b43f61
Mrutyunjay-acadgild <nayak.mrutyunjay3@gmail.com>
2016-12-20 23:19:31 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,19 @@
+var employee ={
+    
+    name : "Mrutyunjay Nayak",
+    age : 23,
+    salary : 18000,
+    address :{
+        
+        city : "Banglore",
+        state : "Karnataka",
+        pincode : 560076
+    }
+}
+
+console.log("Name :"+employee.name);
+console.log("Age :"+employee.age);
+console.log("Salary :"+employee.salary);
+console.log("City :"+employee.address.city);
+console.log("State :"+employee.address.state);
+console.log("Pincode :"+employee.address.pincode);
